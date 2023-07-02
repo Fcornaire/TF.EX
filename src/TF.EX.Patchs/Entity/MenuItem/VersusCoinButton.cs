f1b133ae53f3ebc54c6a564e10d1d5ebe5b29b7b
Francis Cornaire <cornairemax@yahoo.fr>
2023-07-02 09:57:05 +0200

    Revert "Revert "feat: prevent modifying match length""

---
+++ src/TF.EX.Patchs/Entity/MenuItem/VersusCoinButton.cs
@@ -0,0 +1,32 @@
+﻿using TF.EX.Domain.Extensions;
+
+namespace TF.EX.Patchs.Entity.MenuItem
+{
+    internal class VersusCoinButtonPatch : IHookable
+    {
+        public void Load()
+        {
+            On.TowerFall.VersusCoinButton.Update += VersusCoinButton_Update;
+        }
+
+        public void Unload()
+        {
+            On.TowerFall.VersusCoinButton.Update -= VersusCoinButton_Update;
+        }
+
+        private void VersusCoinButton_Update(On.TowerFall.VersusCoinButton.orig_Update orig, TowerFall.VersusCoinButton self)
+        {
+            var currentMode = TowerFall.MainMenu.VersusMatchSettings.Mode;
+
+            if (!currentMode.ToModel().IsNetplay())
+            {
+                orig(self); //Prevent changing on netplay mode
+            }
+            else
+            {
+                //TODO: ux
+            }
+
+        }
+    }
+}
