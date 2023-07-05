b52a732e187d9109d43384236ad47ae8b52a0195
FCornaire <cornairemax@yahoo.fr>
2023-07-05 17:34:44 +0200

    fix: non skippable replay crashing the game

---
+++ src/TF.EX.Patchs/SaveData.cs
@@ -0,0 +1,23 @@
+﻿using TF.EX.TowerFallExtensions;
+
+namespace TF.EX.Patchs
+{
+    internal class SaveDataPatch : IHookable
+    {
+        public void Load()
+        {
+            On.TowerFall.SaveData.Save += Save_Patch;
+        }
+
+        public void Unload()
+        {
+            On.TowerFall.SaveData.Save -= Save_Patch;
+        }
+
+        private string Save_Patch(On.TowerFall.SaveData.orig_Save orig, TowerFall.SaveData self)
+        {
+            self.WithNetplayOptions();
+            return orig(self);
+        }
+    }
+}
