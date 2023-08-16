0b457051e74ace36ba8e20bc2cac501267d9fc9c
Francis Cornaire <cornairemax@yahoo.fr>
2023-08-16 10:30:51 -0400

    fix: Missing HUDFade on Round Result (#82)
    Also fixed typo
    Also prevent connexion dialog appearing only in netplay mode
    Also prevent ReplayRecorder activating on test mode

--- src/TF.EX.Patchs/ReplayRecorder.cs
+++ src/TF.EX.Patchs/ReplayRecorder.cs
@@ -1,9 +1,17 @@
 ﻿using TF.EX.Domain.Extensions;
+using TF.EX.Domain.Ports;
 
 namespace TF.EX.Patchs
 {
     internal class ReplayRecorderPatch : IHookable
     {
+        private readonly INetplayManager _netplayManager;
+
+        public ReplayRecorderPatch(INetplayManager netplayManager)
+        {
+            _netplayManager = netplayManager;
+        }
+
         public void Load()
         {
             On.TowerFall.ReplayRecorder.ClearFrames += ReplayRecorder_ClearFrames;
@@ -18,7 +26,7 @@ namespace TF.EX.Patchs
         {
             var mode = TowerFall.MainMenu.VersusMatchSettings.Mode.ToModel();
 
-            if (mode.IsNetplay())
+            if (mode.IsNetplay() || _netplayManager.GetNetplayMode() == Domain.Models.NetplayMode.Test)
             {
                 /// We don't need Original ReplayRecorder in Netplay
                 /// We can ignore this
