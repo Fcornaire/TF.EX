0f1d8a9a35c367c6c8191444622f18d573468df4
Francis Cornaire <cornairemax@yahoo.fr>
2023-10-01 14:58:47 +0200

    fix: better save player animations (#108)

--- src/TF.EX.Utils/ReplayComparer.cs
+++ src/TF.EX.Utils/ReplayComparer.cs
@@ -1,4 +1,6 @@
 ﻿using DeepEqual.Syntax;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
 using System.Text;
 using TF.EX.Domain.Services;
 using Xunit;
@@ -13,24 +15,33 @@ namespace TF.EX.Utils
         [Fact]
         public async Task TestReplayComparison()
         {
-            string replayFilePath1 = Path.Combine(_replaysFolder, "23-09-2023T14-34-47.tow");
-            string replayFilePath2 = Path.Combine(_replaysFolder, "23-09-2023T14-34-40_laptop.tow");
+            string replayFilePath1 = Path.Combine(_replaysFolder, "01-10-2023T12-46-59.tow");
+            string replayFilePath2 = Path.Combine(_replaysFolder, "01-10-2023T12-46-54_laptop.tow");
 
             List<TF.EX.Domain.Models.Record> record1 = (await ReplayService.ToReplay(replayFilePath1)).Record;
             List<TF.EX.Domain.Models.Record> record2 = (await ReplayService.ToReplay(replayFilePath2)).Record;
 
+            ConcurrentDictionary<int, string> diff = new ConcurrentDictionary<int, string>();
             StringBuilder msgBuilder = new StringBuilder();
             Parallel.ForEach(Enumerable.Range(0, Math.Min(record1.Count(), record2.Count())), i =>
             {
                 try
                 {
+                    record2[i].GameState.MatchStats = record2[i].GameState.MatchStats.Reverse();
                     record1[i].GameState.ShouldDeepEqual(record2[i].GameState);
                 }
                 catch (DeepEqual.Syntax.DeepEqualException e)
                 {
-                    Interlocked.Exchange(ref msgBuilder, new StringBuilder($"{msgBuilder}Diff at frame {i} : {e.Message} \n\n"));
+                    diff.TryAdd(i, $"{msgBuilder}Diff at frame {i} : {e.Message} \n\n");
                 }
             });
+
+            var sorted = diff.ToImmutableSortedDictionary(diff => diff.Key, diff => diff.Value);
+
+            foreach (var item in sorted)
+            {
+                msgBuilder.Append(item.Value);
+            }
 
             string msg = msgBuilder.ToString();
 
@@ -39,6 +50,12 @@ namespace TF.EX.Utils
                 string diffFilePath = Path.Combine(_replaysFolder, "diff.txt");
                 using StreamWriter sw = new StreamWriter(diffFilePath);
                 sw.Write(msg);
+            }
+            else
+            {
+                string diffFilePath = Path.Combine(_replaysFolder, "diff.txt");
+                using StreamWriter sw = new StreamWriter(diffFilePath);
+                sw.Write("No diff!");
             }
         }
 
