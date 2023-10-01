049840cf70fd07ae98196afa03634fc3c1daedcf
Francis Cornaire <cornairemax@yahoo.fr>
2023-10-01 23:28:02 +0200

    fix: wings animations desynch (#110)

---
+++ src/TF.EX.Domain/Models/State/Entity/LevelEntity/Player/PlayerWings.cs
@@ -0,0 +1,9 @@
+﻿namespace TF.EX.Domain.Models.State.Entity.LevelEntity.Player
+{
+    public class PlayerWings
+    {
+        public Sprite<string> Wings { get; set; }
+        public bool IsGaining { get; set; }
+        public float SpriteScaleTweenTimer { get; set; }
+    }
+}
---
+++ src/TF.EX.TowerFallExtensions/CompositeComponent/PlayerWings.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.Utils;
+using TF.EX.Domain.Models.State.Entity.LevelEntity.Player;
+
+namespace TF.EX.TowerFallExtensions.CompositeComponent
+{
+    public static class PlayerWingsExtensions
+    {
+        public static PlayerWings GetState(this TowerFall.PlayerWings compositeComponent)
+        {
+            var dynEntity = DynamicData.For(compositeComponent);
+            var wings = dynEntity.Get<Monocle.Sprite<string>>("sprite");
+            var isGaining = dynEntity.Get<bool>("gaining");
+            Tween tween = compositeComponent.Components.FirstOrDefault(c => c is Tween) as Tween;
+            var spriteScaleTweenTimer = -1f;
+            if (tween != null)
+            {
+                spriteScaleTweenTimer = tween.FramesLeft;
+            }
+
+            return new PlayerWings
+            {
+                Wings = wings != null ? wings.GetState() : null,
+                IsGaining = isGaining,
+                SpriteScaleTweenTimer = spriteScaleTweenTimer,
+            };
+        }
+
+        public static void LoadState(this TowerFall.PlayerWings compositeComponent, PlayerWings toLoad)
+        {
+            var dynEntity = DynamicData.For(compositeComponent);
+
+            var wingsSprite = dynEntity.Get<Monocle.Sprite<string>>("sprite");
+            if (wingsSprite != null && toLoad.Wings != null)
+            {
+                wingsSprite.LoadState(toLoad.Wings);
+            }
+
+            dynEntity.Set("gaining", toLoad.IsGaining);
+
+            compositeComponent.Components.Remove(compositeComponent.Components.FirstOrDefault(c => c is Tween) as Tween);
+
+            if (toLoad.SpriteScaleTweenTimer > 0)
+            {
+                Tween tween = Tween.Create(Tween.TweenMode.Oneshot, Ease.BigBackOut, 40, start: true);
+                var dynTween = DynamicData.For(tween);
+                dynTween.Set("FramesLeft", toLoad.SpriteScaleTweenTimer);
+                tween.OnUpdate = delegate (Tween t)
+                {
+                    var sprite = dynEntity.Get<Monocle.Sprite<string>>("sprite");
+                    sprite.Scale = Vector2.One * t.Eased;
+                };
+                tween.OnComplete = delegate
+                {
+                    dynEntity.Set("gaining", false);
+                };
+                compositeComponent.Add(tween);
+            }
+        }
+    }
+}
